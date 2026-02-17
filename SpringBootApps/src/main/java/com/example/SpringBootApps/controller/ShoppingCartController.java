5591aa4d57eb6b065342c113756f395bac319bb8
Keynocchio <davidvalcarcel1722@gmail.com>
2026-02-16 20:51:09 -0500

    Shopping Cart committed test 1

---
+++ SpringBootApps/src/main/java/com/example/SpringBootApps/controller/ShoppingCartController.java
@@ -0,0 +1,31 @@
+package com.example.SpringBootApps.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringBootApps.entity.ShoppingCart;
+import com.example.SpringBootApps.repository.ShoppingCartRepository;
+
+// controller class handles HTTP requests for Shopping Cart REST API
+@RestController
+@RequestMapping("")
+public class ShoppingCartController {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    public ShoppingCartController(ShoppingCartRepository shoppingCartRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
+    // get all books in a user's shopping cart
+    @GetMapping("/cart/{username}")
+    public List<ShoppingCart> getUserCart(@PathVariable String username) {
+        return shoppingCartRepository.findByUsername(username);
+    }
+}
