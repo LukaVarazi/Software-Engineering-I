ae8e3b8b2d55cb6fefb72a9498afb448b48d68f4
LukaVarazi <lukavarazi@gmail.com>
2026-03-10 20:56:57 -0400

    s

--- SpringBootApps/src/main/java/com/example/SpringBootApps/repository/BookRepository.java
+++ SpringBootApps/src/main/java/com/example/SpringBootApps/repository/BookRepository.java
@@ -1,11 +1,12 @@
 package com.example.SpringBootApps.repository;
-import com.example.SpringBootApps.entity.Book;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.example.SpringBootApps.entity.Book;
 
 
 // repository interface handles database operations for the BOOK entity.
@@ -18,9 +19,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     )
     List<Book> findAllByGenre(@Param("genre") String genre);
 
-        @Query(
-        value = "SELECT * FROM book_table WHERE author_id = :author_id",
-        nativeQuery=true
+    @Query(
+            value = "SELECT * FROM book_table WHERE author_id = :author_id",
+            nativeQuery=true
     )
     List<Book> findByAuthorId(@Param("author_id") Long authorId);
 
