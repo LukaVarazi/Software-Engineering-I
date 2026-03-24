ff18a6b4719dced7b9a76f343ba3839b23687620
Melanie <121034278+vasquezmelanie@users.noreply.github.com>
2026-03-24 16:00:01 -0400

    Update for WishlistController
    Adding a book to a wishlist now works correctly.

--- SpringBootApps/src/main/java/com/example/SpringBootApps/controller/WishlistController.java
+++ SpringBootApps/src/main/java/com/example/SpringBootApps/controller/WishlistController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringBootApps.entity.Book;
@@ -41,10 +40,10 @@ public class WishlistController {
     }
 
     // Add a book
-    @PostMapping("/add-book")
+    @PostMapping("/wishlists/{wishlistId}/books/{bookId}")
     public ResponseEntity<Void> addBookToWishlist(
-        @RequestParam Long wishlistId,
-        @RequestParam Long bookId) {
+        @PathVariable Long wishlistId,
+        @PathVariable Long bookId) {
 
         wishlistService.addBookToWishlist(wishlistId, bookId);
         return ResponseEntity.ok().build();
